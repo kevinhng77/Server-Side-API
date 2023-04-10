16fde8ed64ac65107ca1444a7360871e29b14ed5
kevinhng77 <62964063+kevinhng77@users.noreply.github.com>
2023-04-10 00:54:10 -0700

    added a card and got values to show up off json

--- assets/js.js
+++ assets/js.js
@@ -1,24 +1,26 @@
 var submitButton = document.getElementById('submit-button');
+var inputButton = document.getElementById('yourCity');
 var apiKey= '2c4ad6b204a17feaa8527bcc62401e85';
-var cityName= 'London';
-// var requestUrl= 'api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + apiKey;
+var cityName= 'Sacramento';
+var requestUrl= 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + apiKey;
 
-
-function getData() {
-    var requestUrl = 'https://api.github.com/orgs/nodejs/repos';
+function getData(){
     fetch(requestUrl)
-        .then(function (data) {
-            console.log("data",data);
-        });
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            var iterables = ['0','8','16','24','32']
+            for (let value of iterables){
+                console.log(data.list[value].dt_txt);
+                console.log(data.list[value].main.temp);
+                console.log(data.list[value].wind.speed + " MPH");
+                console.log(data.list[value].main.humidity +" %");
+            }
+        })
 }
 
 
 
-/*
-lnk submit button to event listener to change var cityName
-add eventlistener to submit button to execute getData function
-
-
-*/
 
 submitButton.addEventListener('click', getData);
